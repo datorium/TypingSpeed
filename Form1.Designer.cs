1f8e6af18ba3400d0b1dd91015effa145caf0d1b
Datorium <hello@datorium.lv>
2020-04-20 16:46:28 +0300

    version 16:46

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.labelTextOriginal = new System.Windows.Forms.Label();
             this.textBoxTextType = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pictureBoxClose = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).BeginInit();
             this.SuspendLayout();
             // 
             // labelTextOriginal
@@ -51,14 +51,21 @@
             this.textBoxTextType.Name = "textBoxTextType";
             this.textBoxTextType.Size = new System.Drawing.Size(719, 167);
             this.textBoxTextType.TabIndex = 1;
+            this.textBoxTextType.Click += new System.EventHandler(this.A);
             // 
-            // pictureBox1
+            // pictureBoxClose
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(586, 348);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.pictureBoxClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBoxClose.Image = global::TypingSpeed.Properties.Resources.close_blue;
+            this.pictureBoxClose.Location = new System.Drawing.Point(670, 345);
+            this.pictureBoxClose.Name = "pictureBoxClose";
+            this.pictureBoxClose.Size = new System.Drawing.Size(80, 80);
+            this.pictureBoxClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxClose.TabIndex = 2;
+            this.pictureBoxClose.TabStop = false;
+            this.pictureBoxClose.Click += new System.EventHandler(this.pictureBoxClose_Click);
+            this.pictureBoxClose.MouseEnter += new System.EventHandler(this.pictureBoxClose_MouseEnter);
+            this.pictureBoxClose.MouseLeave += new System.EventHandler(this.pictureBoxClose_MouseLeave);
             // 
             // Form1
             // 
@@ -66,12 +73,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(783, 450);
             this.ControlBox = false;
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBoxClose);
             this.Controls.Add(this.textBoxTextType);
             this.Controls.Add(this.labelTextOriginal);
             this.Name = "Form1";
             this.Text = "Datorium Typing Speed App";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClose)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,7 +88,7 @@
 
         private System.Windows.Forms.Label labelTextOriginal;
         private System.Windows.Forms.TextBox textBoxTextType;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxClose;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,25 @@ namespace TypingSpeed
         {
             InitializeComponent();
         }
+
+        private void pictureBoxClose_MouseEnter(object sender, EventArgs e)
+        {
+            pictureBoxClose.Image = Properties.Resources.close_red;
+        }
+
+        private void pictureBoxClose_MouseLeave(object sender, EventArgs e)
+        {
+            pictureBoxClose.Image = Properties.Resources.close_blue;
+        }
+
+        private void pictureBoxClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void A(object sender, EventArgs e)
+        {
+
+        }
     }
 }
